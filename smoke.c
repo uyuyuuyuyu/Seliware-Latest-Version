7878d0097c1b8092f054d07b54016374e74dbe43
Lei <96663030+uyuyuuyuyu@users.noreply.github.com>
2023-04-15 13:00:07 -0700

    Add files via upload

---
+++ smoke.c
@@ -0,0 +1,204 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+
+#include "uthread.h"
+#include "uthread_mutex_cond.h"
+
+#define NUM_ITERATIONS 1000
+
+#ifdef VERBOSE
+#define VERBOSE_PRINT(S, ...) printf (S, ##__VA_ARGS__)
+#else
+#define VERBOSE_PRINT(S, ...) ((void) 0) // do nothing
+#endif
+
+struct Agent {
+  uthread_mutex_t mutex;
+  uthread_cond_t  match;
+  uthread_cond_t  paper;
+  uthread_cond_t  tobacco;
+  uthread_cond_t  smoke;
+};
+
+struct Agent* createAgent() {
+  struct Agent* agent = malloc (sizeof (struct Agent));
+  agent->mutex   = uthread_mutex_create();
+  agent->paper   = uthread_cond_create(agent->mutex);
+  agent->match   = uthread_cond_create(agent->mutex);
+  agent->tobacco = uthread_cond_create(agent->mutex);
+  agent->smoke   = uthread_cond_create(agent->mutex);
+  return agent;
+}
+
+//
+// TODO
+// You will probably need to add some procedures and struct etc.
+//
+
+
+/**
+ * You might find these declarations helpful.
+ *   Note that Resource enum had values 1, 2 and 4 so you can combine resources;
+ *   e.g., having a MATCH and PAPER is the value MATCH | PAPER == 1 | 2 == 3
+ */
+enum Resource            {    MATCH = 1, PAPER = 2,   TOBACCO = 4};
+char* resource_name [] = {"", "match",   "paper", "", "tobacco"};
+
+struct Smoker {
+  struct Agent* agent;
+  enum Resource resource;
+};
+
+
+// # of threads waiting for a signal. Used to ensure that the agent
+// only signals once all other threads are ready.
+int num_active_threads = 0;
+int resources_on_table = 0;
+
+int signal_count [5];  // # of times resource signalled
+int smoke_count  [5];  // # of times smoker with resource smoked
+
+void* smoker(void* v) {
+  struct Smoker* s = v;
+  struct Agent* a = s->agent;
+  enum Resource resource = s->resource;
+
+  uthread_mutex_lock(a->mutex);
+  num_active_threads++;
+
+  while (1) {
+    if ((resources_on_table & ~resource) == (MATCH | PAPER | TOBACCO) - resource) {
+      resources_on_table = 0;
+      smoke_count[resource]++;
+      VERBOSE_PRINT("smoker %s smoked\n", resource_name[resource]);
+      uthread_cond_signal(a->smoke);
+    } else {
+      switch (resource) {
+        case MATCH:
+          uthread_cond_wait(a->match);
+          break;
+        case PAPER:
+          uthread_cond_wait(a->paper);
+          break;
+        case TOBACCO:
+          uthread_cond_wait(a->tobacco);
+          break;
+      }
+      resources_on_table |= resource;
+    }
+  }
+
+  uthread_mutex_unlock(a->mutex);
+  return NULL;
+}
+
+/**
+ * This is the agent procedure.  It is complete and you shouldn't change it in
+ * any material way.  You can modify it if you like, but be sure that all it does
+ * is choose 2 random resources, signal their condition variables, and then wait
+ * wait for a smoker to smoke.
+ */
+void* agent (void* av) {
+  struct Agent* a = av;
+  static const int choices[]         = {MATCH|PAPER, MATCH|TOBACCO, PAPER|TOBACCO};
+  static const int matching_smoker[] = {TOBACCO,     PAPER,         MATCH};
+
+  srandom(time(NULL));
+  
+  uthread_mutex_lock (a->mutex);
+  // Wait until all other threads are waiting for a signal
+  while (num_active_threads < 3)
+    uthread_cond_wait (a->smoke);
+
+  for (int i = 0; i < NUM_ITERATIONS; i++) {
+    int r = random() % 6;
+    switch(r) {
+    case 0:
+      signal_count[TOBACCO]++;
+      VERBOSE_PRINT ("match available\n");
+      uthread_cond_signal (a->match);
+      VERBOSE_PRINT ("paper available\n");
+      uthread_cond_signal (a->paper);
+      break;
+    case 1:
+      signal_count[PAPER]++;
+      VERBOSE_PRINT ("match available\n");
+      uthread_cond_signal (a->match);
+      VERBOSE_PRINT ("tobacco available\n");
+      uthread_cond_signal (a->tobacco);
+      break;
+    case 2:
+      signal_count[MATCH]++;
+      VERBOSE_PRINT ("paper available\n");
+      uthread_cond_signal (a->paper);
+      VERBOSE_PRINT ("tobacco available\n");
+      uthread_cond_signal (a->tobacco);
+      break;
+    case 3:
+      signal_count[TOBACCO]++;
+      VERBOSE_PRINT ("paper available\n");
+      uthread_cond_signal (a->paper);
+      VERBOSE_PRINT ("match available\n");
+      uthread_cond_signal (a->match);
+      break;
+    case 4:
+      signal_count[PAPER]++;
+      VERBOSE_PRINT ("tobacco available\n");
+      uthread_cond_signal (a->tobacco);
+      VERBOSE_PRINT ("match available\n");
+      uthread_cond_signal (a->match);
+      break;
+    case 5:
+      signal_count[MATCH]++;
+      VERBOSE_PRINT ("tobacco available\n");
+      uthread_cond_signal (a->tobacco);
+      VERBOSE_PRINT ("paper available\n");
+      uthread_cond_signal (a->paper);
+      break;
+    }
+    VERBOSE_PRINT ("agent is waiting for smoker to smoke\n");
+    uthread_cond_wait (a->smoke);
+  }
+  
+  uthread_mutex_unlock (a->mutex);
+  return NULL;
+}
+
+int main (int argc, char** argv) {
+  
+  struct Agent* a = createAgent();
+  uthread_t agent_thread;
+  uthread_t smokers[3];
+
+  uthread_init(5);
+  
+  // TODO
+
+    struct Smoker smokers_data[3] = {
+    { .agent = a, .resource = MATCH },
+    { .agent = a, .resource = PAPER },
+    { .agent = a, .resource = TOBACCO }
+  };
+
+
+  agent_thread = uthread_create(agent, a);
+
+   for (int i = 0; i < 3; i++) {
+    smokers[i] = uthread_create(smoker, &smokers_data[i]);
+  }
+  uthread_join(agent_thread, NULL);
+
+  assert (signal_count [MATCH]   == smoke_count [MATCH]);
+  assert (signal_count [PAPER]   == smoke_count [PAPER]);
+  assert (signal_count [TOBACCO] == smoke_count [TOBACCO]);
+  assert (smoke_count [MATCH] + smoke_count [PAPER] + smoke_count [TOBACCO] == NUM_ITERATIONS);
+
+  printf ("Smoke counts: %d matches, %d paper, %d tobacco\n",
+          smoke_count [MATCH], smoke_count [PAPER], smoke_count [TOBACCO]);
+
+  return 0;
+}
